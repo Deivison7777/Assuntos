d8a1fa191175730f47720bbe4b5baa1465a56e25
Deivison7777 <143571154+Deivison7777@users.noreply.github.com>
2023-11-09 15:01:52 -0300

    Vetor Monstro

---
+++ vetores5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+int i;
+int tamanho = 3;
+float notas [tamanho];
+float media;
+int soma = 0;
+printf("Insira as nota do usuario \n");
+
+for ( i = 0; i < tamanho; i++)
+{
+    printf("Nota %d°: ", i+1);
+    scanf("%f", &notas[i]);
+
+soma += notas[i];
+media = soma / tamanho;
+
+}
+
+printf("Mostrando as notas \n");
+for ( i = 0; i < tamanho; i++)
+{
+    printf("Nota %d: %.1f \n", i+1,notas [i]);
+}
+printf("A media e: %.2f", media);
+    return 0;
+}
