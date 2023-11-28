4ef660ecbb2684b0dff4406c38d62ea35248d401
Deivison7777 <143571154+Deivison7777@users.noreply.github.com>
2023-11-28 16:53:34 -0300

    Resolvendo.c

--- Atividade2.c
+++ Atividade2.c
@@ -4,53 +4,58 @@
 int main()
 {
 
-    char disciplina[3][1000];
-    float notas[3][2];
-    int i, j;
+    char nome[5][1000];
+    int idade[5][5];
+    float notas[5][3];
+    int i;
+    int j;
     int soma = 0;
-    float media[2];
+    float media[5];
 
-    for (i = 0; i < 3; i++)
+    for (i = 0; i < 5; i++)
     {
-        printf("\nNome da %d° disciplina: ", i + 1);
-        gets(disciplina[i]);
-        for (j = 0; j < 2; j++)
+        fflush(stdin);
+        printf("Nome do %d° aluno: ", i + 1);
+        gets(nome[i]);
+
+        printf("Idade: ");
+        scanf("%d", &idade[i]);
+
+        for (j = 0; j < 3; j++)
         {
-            printf("digite a %d° nota: ", j + 1);
+            printf("Digite a %d° nota: ", j + 1);
             scanf("%f", &notas[i][j]);
 
             soma += notas[i][j];
         }
-        media[i] = soma / j;
-        soma = 0;
-        fflush(stdin);
+    media[i] = soma / j;
+    soma = 0;
+    fflush(stdin);
     }
 
-    printf("\n Exibindo dados\n");
-
-    for (i = 0; i < 3; i++)
+    printf("\n Exibindo dados...\n");
+    for (i = 0; i < 5; i++)
     {
-        printf("%d Disciplina: %s \n\n", i + 1, disciplina[i]);
-   
-   for ( j = 0; j < 2; j++)
-   {
-    printf("nota do %d° aluno: %.2f \n\n", j+1, notas[i][j]);
+        printf(" %d° Aluno: %s \n", i + 1, nome[i]);
+        printf("\nIdade: %d \n", idade[i]);
+        for (j = 0; j < 3; j++)
+        {
+            printf("\nnota do %d° aluno: %.2f \n\n", j + 1, notas[i][j]);
+        }
+            printf("\nMedia: %.1f \n", media[i]);
 
-   }
-   printf("Media: %.2f \n", media[i]);
-
-if (media[i] >= 7.0)
-{
-    printf("\nAproveido!\n");
-}
-else if (media[i] >= 5.0 )
-{
-    printf("\nRecu recu!\n");
-}
-else
-{
-    printf("\nReprovado!\n");
-}
+        if (media[i] >= 7)
+        {
+            printf("\nAprovado!\n");
+        }
+        else if (media[i] >= 5)
+        {
+            printf("\nRecuperacao!\n");
+        }
+        else
+        {
+            printf("\nReprovado!\n");
+        }
     }
 
     return 0;
