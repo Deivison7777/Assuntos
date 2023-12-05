96096151825e165dc0bf09655544fb163c40c060
Deivison7777 <143571154+Deivison7777@users.noreply.github.com>
2023-12-05 13:57:57 -0300

    função base.c

---
+++ função base.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void cabecalho() {
+    printf("\n===================================\n");
+    printf("\tSENAI");
+    printf("\n===================================\n");
+}
+
+int main (){
+    
+    char nome[200];
+    int idade;
+    
+   
+    cabecalho();
+    printf("Digite um nome: ");
+    gets(nome);
+    
+    printf("Digite uma idade: ");
+    scanf("%d", &idade);
+    
+    cabecalho();
+    
+    printf("Nome: %s \n", nome);
+    printf("Idade: %d \n", idade);
+    
+    return 0;
+}
+
